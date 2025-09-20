a4d476768a47fb7978abe27497caa216cd4eeb48
he1snber8 <116442582+he1snber8@users.noreply.github.com>
2025-09-20 17:49:23 +0400

    Add tasksController.js

---
+++ controllers/tasksController.js
@@ -0,0 +1,4 @@
+// controllers/tasksController.js
+let tasks = [];
+exports.getTasks = (req, res) => { /* sort by priority and return */ };
+exports.createTask = (req, res) => { /* add task with projectId and priority */ };
