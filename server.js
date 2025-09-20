79c28689564889ac7372beb1770b6b87eb5b42cf
he1snber8 <116442582+he1snber8@users.noreply.github.com>
2025-09-20 17:49:18 +0400

    Add server.js

---
+++ server.js
@@ -0,0 +1,6 @@
+// server.js
+const express = require('express');
+require('dotenv').config();
+const app = express();
+app.use(express.json());
+app.listen(process.env.PORT, () => console.log(`Server running on ${process.env.PORT}`));
